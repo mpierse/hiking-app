d362465936fad4db3366dcf9917416b5481feba6
mpierse <maepierse@gmail.com>
2019-01-10 16:39:34 -0500

    added mock data for all 3 locations

---
+++ src/app/wilm-trails/mock-wilm-trails.ts
@@ -0,0 +1,9 @@
+import {Trails} from '../trails';
+
+export const WILMTRAILS: Trails[] = [
+    {id: 1, name : 'Whitely Farm Loop'},
+    {id: 2, name : 'Another Loop'},
+    {id: 3, name : 'And Another Loop'},
+    {id: 4, name : 'Next Loop'},
+    {id: 5, name : 'Trail Loop'},
+];
